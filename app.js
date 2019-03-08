62f94ec14e44b26d6028f995fc042d561eb4a24f
Gerson Cruz <gcruz1776@gmail.com>
2019-03-07 20:29:22 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,94 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Set variable for reservation and wait list arrays
+var reservation = [];
+var waitList = [];  
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Restaurant DATA 
+// =============================================================
+
+var guestList = [
+    {
+        name: "Jason Bourne"
+    },
+
+    {
+        name: "Jane Doe"
+    },
+
+    {
+        name: "Eli Manning"
+    }
+]
+
+var putonWaitingList = [
+    {
+        name:"Darth Vadar"
+    },
+
+    {
+        name:"Yoshi"
+    }
+]
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+  
+  app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+
+  app.get("/reservations", function(req, res) {
+    res.sendFile(path.join(__dirname, "reservations.html"));
+  });
+  
+ 
+
+// Displays all Reservations
+app.get("/api/reservations", function(req, res) {
+    return res.json(guestList);
+  });
+  
+  // Display waitlist
+  app.get("/api/waitlist", function(req, res) {
+   
+    return res.json(waitList);
+  });
+
+
+//   // Create New Guest -- take in JSON input
+
+//   app.post("/api/reservations", function(req, res) {
+//     var newGuest = req.body;
+
+//     newGuest.routeName = newGuest.name.replace(/\s+/g, "").toLowerCase();
+
+//     console.log(newGuest);
+//     newGuest.push(guestList);
+//     res.json(newGuest);
+//   });
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
